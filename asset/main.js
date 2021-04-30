4cffcac5307ca97fbe1678f37824c2f97762fb68
Wiam Malek <wiam.malek95@gmail.com>
2021-04-30 14:44:58 +0200

    ajout

--- asset/main.js
+++ asset/main.js
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
- 
-  $(document).ready(function(){
-    const table = [
-        {
-            name : "Wiam",
-            fruit : "Orange",
-            animal : "dog"
-        },
-    ];
-    });
-
-=======
 $(document).ready(function(){
 const table = [
   {
-    name: "Saphir",
-    fruit: "Kiwi",
-    animal: "Koala",
+    name : "Wiam",
+    fruit : "Orange",
+    animal : "dog"
   },
   {
     name: "Louiza",
@@ -30,4 +17,4 @@ const table = [
   },
 ];
 });
->>>>>>> 33418f658d45bf85f54b965754f7828d566a5e62
+
