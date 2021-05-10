74372d60d0e9947ab16e572d975b88f156ab6c7d
olga <>
2021-05-10 20:22:34 +0200

    add jquery methods, add script

--- asset/main.js
+++ asset/main.js
@@ -26,5 +26,23 @@ const table = [
     animal: "Cat",
   },
 ];
+const createTd = (key, value) => {
+  const $el = document.createElement("td");
+  $el.classList.add(`input-${key}`);
+  $el.innerText = value;
+  return $el;
+};
+
+table.forEach((input) => {
+  const $input = document.createElement("tr");
+  $input.classList.add("input");
+  //$input.
+  $input.appendChild(createTd("name", input.name));
+  $input.appendChild(createTd("fruit", input.fruit));
+  $input.appendChild(createTd("animal", input.animal));
+
+  document.querySelector("tbody").appendChild($input);
 });
 
+
+});
